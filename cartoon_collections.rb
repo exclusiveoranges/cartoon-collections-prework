177764cba98ed03a932f642c94d508b5da666f34
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 02:12:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,11 +16,11 @@ end
 #
 # end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
+#
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
