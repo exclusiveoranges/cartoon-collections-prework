10434eeb70124d4d1b64df8c75aa516487f237ea
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 02:48:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |element|
+  variable = cheese_types.each do |element|
     array.find(element)
   end
 end
