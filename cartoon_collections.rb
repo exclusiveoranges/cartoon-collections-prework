50b3834e35382849a94a8662937cbaae43371091
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 02:31:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(array)
     if element.size > 4
       true
     else
-      false 
-end
+      false
+  end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
