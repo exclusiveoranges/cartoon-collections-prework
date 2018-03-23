65100b79475f430cfa329a7b2b46aabf66a6ce39
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 02:43:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(array)
     end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find("cheddar" || "gouda" || "camembert")
 end
