e0e32e2bac1a8b12f767f693696d805d1508ae6d
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 02:10:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,14 +8,14 @@ end
 
 end
 
-def summon_captain_plane(array)
-  new_array = []
-  array.each do |element|
-  variable = element.capitalize
-  end
-  new_array.push(variable)
-
-end
+# def summon_captain_plane(array)
+#   new_array = []
+#   array.each do |element|
+#   variable = element.capitalize
+#   end
+#   new_array.push(variable)
+#
+# end
 
 def long_planeteer_calls# code an argument here
   # Your code here
